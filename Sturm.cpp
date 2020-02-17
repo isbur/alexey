e1fe582a1f54d91d13399e529fa023a1375f5eeb
Igor Burenkov <isburenkov@gmail.com>
2020-02-17 11:48:48 +0000

    зкÑpre dP

--- Sturm.cpp
+++ Sturm.cpp
@@ -1,12 +1,12 @@
-void derivative(){
-
+vector<double> derivative(P){
+    return vector<double> dP()
 }
 
 void divide_polynom_A_by_polynom_B(A, B){
 
 }
 
-void construct_Sturm_chain(polynom P) {
+void construct_Sturm_chain(vector<double> P) {
     P_0 = P;
     P_1 = derivative(P);
     i = 2;
