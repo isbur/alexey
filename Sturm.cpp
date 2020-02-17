d105be6f57ea2851790850a2b802c166ee0732c7
Igor Burenkov <isburenkov@gmail.com>
2020-02-17 13:13:28 +0000

    started to construct Sturm chains

--- Sturm.cpp
+++ Sturm.cpp
@@ -14,14 +14,14 @@ vector<double> derivative(vector<double> P){
 void divide_polynom_A_by_polynom_B(A, B){
 
 }
+*/
 
 void construct_Sturm_chain(vector<double> P) {
-    P_0 = P;
-    P_1 = derivative(P);
+    vector<double> P_0 = P;
+    vector<double> P_1 = derivative(P);
     i = 2;
     while(new_polynom != 1){
         new_polynom = divide_polynom_A_by_polynom_B(P_i, P_i+1);
         i++;
     }
 }
-*/
