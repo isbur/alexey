a2cb75a11b7576c5a2f2c7c148c035db1413885a
Igor Burenkov <isburenkov@gmail.com>
2020-02-17 07:52:11 +0000

    unittests et boost trial

--- 2.cpp
+++ 2.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
 using namespace std;
 #include "Sturm.cpp"
 
 
-int main() {
-    ifstream input_file;
-    double t, d1, d2;
-    double* Coefficients;
+int read_data(double* t, pair <double, double>* boundaries, polynomial <double>** P);
 
-    input_file.open("input.txt");
-    // read_accuracy()
-    input_file >> t;
-    // read_interval_boundaries();
-    input_file >> d1 >> d2;
-    // read_coefficients();
-    Coefficients = 
+
+int main() {
+    
+    double t;
+    pair <double, double> boundaries;
+    polynomial <double>* Polynomials;
+
+    read_data(&t, &boundaries, &Polynomials); 
 
 
     construct_Sturm_chain(Coefficients);
 
     return(0);
+}
+
+
+int read_data(double* t, pair <double, double>* boundaries, polynomial <double>** Polynomials){
+
+    ifstream input_file;
+
+    input_file.open("input.txt");
+
+    // read_accuracy()
+    input_file >> *t;
+    // read_interval_boundaries();
+    input_file >> *d1 >> *d2;
+    // read_coefficients();
+
+
+    input_file.close();
+
+    return 0;
 }
