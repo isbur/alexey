a91919a035668c2108798b5c38695a38bd8ca7cf
Igor Burenkov <isburenkov@gmail.com>
2020-02-17 08:19:31 +0000

    Succesfully passed vectors to function by reference; got rid of Boost

--- 2.cpp
+++ 2.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <utility>
+#include <vector>
 using namespace std;
-#include "Sturm.cpp"
+//#include "Sturm.cpp"
 
 
-int read_data(double* t, pair <double, double>* boundaries, polynomial <double>** P);
+int read_data(double &t, pair <double, double> &boundaries, vector <vector <int> > &Polynomials);
 
 
 int main() {
     
     double t;
     pair <double, double> boundaries;
-    polynomial <double>* Polynomials;
+    vector < vector <int> > Polynomials;
 
-    read_data(&t, &boundaries, &Polynomials); 
+    read_data(t, boundaries, Polynomials); 
 
 
-    construct_Sturm_chain(Coefficients);
+    //construct_Sturm_chain(Coefficients);
 
     return(0);
 }
 
 
-int read_data(double* t, pair <double, double>* boundaries, polynomial <double>** Polynomials){
+int read_data(double &t, pair <double, double> &boundaries, vector <vector <int> > &Polynomials){
 
     ifstream input_file;
+
 
     input_file.open("input.txt");
 
     // read_accuracy()
-    input_file >> *t;
+    input_file >> t;
     // read_interval_boundaries();
-    input_file >> *d1 >> *d2;
+    input_file >> boundaries.first >> boundaries.second;
     // read_coefficients();
 
 
