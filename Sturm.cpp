d1621a4ae555934a4132d20382648fea66b69d9d
Igor Burenkov <isburenkov@gmail.com>
2020-02-17 13:20:59 +0000

    only polynomial devision remained to finish Strum chain construction

--- Sturm.cpp
+++ Sturm.cpp
@@ -17,11 +17,12 @@ void divide_polynom_A_by_polynom_B(A, B){
 */
 
 void construct_Sturm_chain(vector<double> P) {
-    vector<double> P_0 = P;
-    vector<double> P_1 = derivative(P);
+    vector <vector <double> > Sturm_chain;
+    Sturm_chain.push_back(P);
+    Sturm_chain.push_back( derivative(P) );
     i = 2;
     while(new_polynom != 1){
-        new_polynom = divide_polynom_A_by_polynom_B(P_i, P_i+1);
+        new_polynom = divide_polynom_A_by_polynom_B(P[i-1], P[i]);
         i++;
     }
 }
