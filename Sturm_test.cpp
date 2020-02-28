f1919bca97dfaef2e6fd880284734dfc0a3c0e30
Igor Burenkov <isburenkov@gmail.com>
2020-02-28 19:26:26 +0000

    test

--- Sturm_test.cpp
+++ Sturm_test.cpp
@@ -5,15 +5,15 @@
 TEST_CASE("Sturm chain is constructed", "[Sturm]"){
     
     Poly a{1, -4, 3};
-    Poly b{2, -4, 0};
-    Poly c{0.25, -0, 0};
+    Poly b{2, -4};
+    Poly c{0.25};
     Sturm_chain A = construct_Sturm_chain(
-            Poly_plus{Poly{1, -4, 3}, 2}
+            Poly{1, -4, 3}
     );
     Sturm_chain B{
-        Poly_plus{Poly{1, -4, 3}, 2},
-        Poly_plus{Poly{2, -4, 0}, 1},
-        Poly_plus{Poly{0.25, -0}, 0}        
+        Poly{1, -4, 3},
+        Poly{2, -4},
+        Poly{0.25}        
     } ;
     REQUIRE(A == B);
     REQUIRE(A.size() == 3);
